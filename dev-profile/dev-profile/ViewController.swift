0e4698d92d823c75a2719370ed9cf0440d372dce
Pulkit Gulati <pulkit.gemini@gmail.com>
2018-05-03 23:54:10 +0530

    first commit

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Pulkit Gulati on 03/05/18.
+//  Copyright © 2018 Pulkit Gulati LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
